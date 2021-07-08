0d21872023a5c174036432dbd22fef1800e9c795
Shachar Nissan <Shachar208@gmail.com>
2021-07-08 12:25:34 +0300

    table activity added

---
+++ app/src/main/java/com/classy/class_2021a_and_10/Activity_UnlockTable.java
@@ -0,0 +1,14 @@
+package com.classy.class_2021a_and_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_UnlockTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock_table);
+    }
+}
---
+++ app/src/main/java/com/classy/class_2021a_and_10/UnlockItem.java
@@ -0,0 +1,23 @@
+package com.classy.class_2021a_and_10;
+
+public class UnlockItem {
+    private int image;
+    private String date;
+
+    public UnlockItem(int image, String date){
+        this.image = image;
+        this.date = date;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
